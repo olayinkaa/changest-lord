a50b0c6d66130f6ff869018a0ac857fbdc309c66
olayinka.ibrahim <olayinka.ibrahim@etranzact.com>
2026-08-26 09:39:53 +0100

    code updated

--- src/modules/onboarding/onboarding.service.ts
+++ src/modules/onboarding/onboarding.service.ts
@@ -196,6 +196,7 @@ export class OnboardingService implements IOnboardingService {
 		}
 		let userId5 = user.userId5
 
+		// if (user.userType === UserType.seller && !userId5) {
 		if (user.userType === UserType.seller && !userId5) {
 			userId5 = await this.utilityService.generateUniqueUserId5()
 		}
